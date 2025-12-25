62e31b680b1fae476b2fd1ce13a9e3825f99108a
jaypandya594 <74663733+jaypandya594@users.noreply.github.com>
2025-12-25 15:36:43 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-  }
-  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+def call(String projectName, String imageTag, String dockerHubUser) {
+    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "docker push ${dockerHubUser}/${projectName}:${imageTag}"
+    }
 }
